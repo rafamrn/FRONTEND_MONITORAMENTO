d2ce57127c4bffa01534b8c0249a165009be8427
r1m2@GITHUB <rafaelmorenods@gmail.com>
2025-05-01 20:07:30 -0300

    recuperacao

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backendmonitoramento-production.up.railway.app", // Altere para a URL do backend em produção
+  baseURL: "http://127.0.0.1:8000", // Altere para a URL do backend em produção
   headers: {
     "Content-Type": "application/json",
   },
